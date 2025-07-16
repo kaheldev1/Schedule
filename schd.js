5e6fed1f2b6aa9a4b966f015458d126d19066336
kaheldev1 <keltilaon8@gmail.com>
2025-07-16 18:39:44 +0800

    Delete schd.js

--- schd.js
+++
@@ -1,5 +0,0 @@
-// JavaScript code for your schedule page
-console.log("Schedule page loaded!");
-
-// You can add more JavaScript functionality here if needed,
-// for example, dynamic content loading, interactive elements, etc.
